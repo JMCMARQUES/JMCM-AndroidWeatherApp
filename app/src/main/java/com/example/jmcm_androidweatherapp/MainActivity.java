b5cdbe5677248eb30d63313d3287bda14322c83e
João Marques <joao.miguel.marques@novabase.pt>
2019-06-25 18:07:06 +0100

    added spinner with predifined set of values -  working

--- app/src/main/java/com/example/jmcm_androidweatherapp/MainActivity.java
+++ app/src/main/java/com/example/jmcm_androidweatherapp/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.jmcm_androidweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.spinner)
+    public Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        spinnerLoad();
+
     }
+
+
+
+    public void spinnerLoad(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.cityList,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/jmcm_androidweatherapp/WeatherAPIRequest.java
@@ -0,0 +1,4 @@
+package com.example.jmcm_androidweatherapp;
+
+public class WeatherAPIRequest {
+}
