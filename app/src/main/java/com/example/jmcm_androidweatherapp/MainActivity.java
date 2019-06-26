3cfbe8a87e121d50fb4c30438a3464252c088057
João Marques <joao.miguel.marques@novabase.pt>
2019-06-26 16:28:04 +0100

    essential code implemented. problems at getting the connection code status, the app crashes when the flow reaches this method

--- app/src/main/java/com/example/jmcm_androidweatherapp/MainActivity.java
+++ app/src/main/java/com/example/jmcm_androidweatherapp/MainActivity.java
@@ -5,36 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private WeatherAPIRequest weatherAPIRequest;
+
     @BindView(R.id.spinner)
     public Spinner spinner;
+
+    @BindView(R.id.textView)
+    public TextView textView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
 
+
         spinnerLoad();
+        weatherAPIRequest = new WeatherAPIRequest();
 
     }
 
 
-
-    public void spinnerLoad(){
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.cityList,android.R.layout.simple_spinner_item);
+    public void spinnerLoad() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cityList, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
     }
 
+
+    @OnClick(R.id.button)
+    public void getData() {
+
+        String city = spinner.getSelectedItem().toString();
+
+        weatherAPIRequest.myGetRequest(city);
+
+        textView.setText(city);
+
+    }
 
 
 }
--- app/src/main/java/com/example/jmcm_androidweatherapp/WeatherAPIRequest.java
+++ app/src/main/java/com/example/jmcm_androidweatherapp/WeatherAPIRequest.java
@@ -1,4 +1,72 @@
 package com.example.jmcm_androidweatherapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class WeatherAPIRequest {
+
+
+    public String myGetRequest(String city) {
+
+        String readLine = null;
+        String toResponse = null;
+        String[] toResponseArray = new String[]{};
+
+        try {
+            URL getRequestURL = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=5e41726cfce9965c4d22634201bd1e83");
+            HttpURLConnection connection = (HttpURLConnection) getRequestURL.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            //connection.setReadTimeout(3000);
+            System.out.println("printing something: " + connection);
+            //connection.connect();
+            //System.out.println("printing something: ");
+
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("entered if statement"  + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuffer response = new StringBuffer();
+
+
+                while ((readLine = in.readLine()) != null) {
+                    response.append(readLine);
+                }
+                in.close();
+                connection.disconnect();
+
+                toResponse = response.toString();
+                toResponseArray = toResponse.split(",");
+
+                System.out.println("JSON String Result" + toResponse);
+
+            } else {
+                System.out.println("GET didn't work");
+            }
+
+
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            System.out.println("failed to make connection: " + e.getMessage());
+            e.printStackTrace();
+            e.getMessage();
+            e.getCause();
+        }
+
+
+        return toResponseArray[3];
+
+    }
+
 }
